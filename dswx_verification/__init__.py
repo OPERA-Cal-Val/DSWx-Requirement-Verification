0e172f8b0967b3e53969d86e39e89ceaa03e8941
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2023-09-28 14:21:47 -0700

    update version dynamically

--- dswx_verification/__init__.py
+++ dswx_verification/__init__.py
@@ -1,6 +1,10 @@
+import warnings
+from importlib.metadata import PackageNotFoundError, version
+
 from .labels import (reclassify_validation_dataset_to_dswx_frame,
                      resample_label_into_percentage)
-from .latex import get_main_beamer_tex_template, get_slide_tex_template, render_latex_template
+from .latex import (get_main_beamer_tex_template, get_slide_tex_template,
+                    render_latex_template)
 from .mask_utils import (get_contiguous_areas_of_class_with_maximum_size,
                          get_number_of_pixels_in_hectare)
 from .metrics import get_all_metrics_for_one_trial
@@ -11,6 +15,14 @@ from .val_db import (generate_linked_id_table_for_classified_imagery,
                      get_localized_validation_table,
                      get_path_of_validation_geojson,
                      get_validation_metadata_by_site_name)
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = None
+    warnings.warn('package is not installed!\n'
+                  'Install in editable/develop mode via (from the top of this repo):\n'
+                  '   python -m pip install -e .\n', RuntimeWarning)
 
 __all__ = ['generate_linked_id_table_for_classified_imagery',
            'get_path_of_validation_geojson',
