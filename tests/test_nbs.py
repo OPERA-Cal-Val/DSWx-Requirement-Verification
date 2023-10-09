d47e300b175b24181ce4b6c84ad26a4ea7e882a5
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2023-10-09 16:07:30 -0700

    comments

--- tests/test_nbs.py
+++ tests/test_nbs.py
@@ -12,6 +12,7 @@ def test_notebook_integration(verify_all_script_path):
     result = runner.invoke(main, ['--yaml_config', f'{repo_dir}/verification_parameters.yml',
                                   '--output_notebooks_dir', None,
                                   '--sites', '3_10'])
+    # This ensures we can more easily see output of CLI
     print(result.stdout)
     print(result.exception)
     assert result.exit_code == 0
