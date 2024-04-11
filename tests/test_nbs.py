2e403cb919ca862c34a402f3ce04fdda8c515fc5
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2024-04-11 09:50:12 -0700

    remove weird dir for test

--- tests/test_nbs.py
+++ tests/test_nbs.py
@@ -13,7 +13,7 @@ def test_notebook_integration(verify_all_script_path,
 
     runner = CliRunner()
     result = runner.invoke(main, ['--yaml_config', str(yaml_config_path),
-                                  '--output_notebooks_dir', 'testing1234',
+                                  '--output_notebooks_dir', None,
                                   '--sites', '3_10'])
     # This ensures we can more easily see output of CLI
     print(result.stdout)
